82d4a8886f050d4d1947f55da057fc0954b02525
Ecocoder0711 <sakshamkashyapgame80@gmail.com>
2025-06-16 23:34:00 +0530

    update 1.10

--- App_GUI.py
+++ App_GUI.py
@@ -9,7 +9,7 @@ Base_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 def get_weather():
     city = city_entry.get().strip() #Get city for input
-    if not ciy:
+    if not city:
         messagebox.showwarning("Input Error!","Enter the Correct city.")
 
     url = f"{Base_URL}?q={city}&appid={api_key}&units=metric"
@@ -18,8 +18,8 @@ def get_weather():
         response = requests.get(url)
         data = response.json()
 
-        if data["Cod"] == 200:
-            city_name = data[name]
+        if data["cod"] == 200:
+            city_name = data["name"]
             temperature= data["main"]["temp"]
             description= data["weather"][0]["description"]
             humidity= data["main"]["humidity"]
@@ -56,6 +56,27 @@ def get_weather():
                 bg = bg_color,
                 font = ("courier","11","bold")
             )
+        else:
+            messagebox.showerror("Error!","City not found.")
+    except requests.exceptions.RequestException as e:
+        messagebox.showerror("❌ Network Error", f"Failed to connect: {e}")
+
+# Setup the GUI Window
+window = tk.Tk()
+window.title("The Weather App")
+window.geometry("400x350") # here 400 is width and 350 is height
+
+tk.Label(window,text= "Enter City", font = ("Halvetica" , 12)).pack(pady=10)
+city_entry = tk.Entry(window,font= ("Halvetica",12))
+city_entry.pack(pady=10)
+
+tk.Button(window,text = "Get Weather", command= get_weather).pack(pady=10)
+result_label = tk.Label(window, text="",font = ("Halvetica",12),justify = "left")
+result_label.pack(pady=10)
+
+tk.Button(window,text = "Quit" , command = window.quit).pack(pady=10)
+
+window.mainloop()
 
 
 
